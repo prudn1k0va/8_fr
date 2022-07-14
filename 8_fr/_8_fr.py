5ea3903e51f6bfbd579cabe6c968c1c611d64cc2
prudn1k0va <aristvodolaz@mail.ru>
2022-07-14 17:44:40 +0300

    second com

--- 8_fr/_8_fr.py
+++ 8_fr/_8_fr.py
@@ -11,3 +11,17 @@ def f(k, e):
             print(('-' * x) + '*' + ('-' * (7 - x)))
         sols += 1
         return
+    for i in range(8):
+        for x, y in e:
+            if x == i or abs(y - k) == abs(x - i):
+                break
+        else:
+            new_e = list(e)
+            new_e.append((i, k))
+            f(k + 1, new_e)
+
+def main():
+    f(0, [])
+    print("solutions:", sols)
+
+main()
