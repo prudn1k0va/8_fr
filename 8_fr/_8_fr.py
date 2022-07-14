b56a683f61ad92bb930c001c833041fbe1f67792
prudn1k0va <aristvodolaz@mail.ru>
2022-07-14 17:42:12 +0300

    first

--- 8_fr/_8_fr.py
+++ 8_fr/_8_fr.py
@@ -1,1 +1,13 @@
 
+from random import randint
+
+sols = 0
+
+def f(k, e):
+    global sols
+    if k == 8:
+        print(e)
+        for x, _ in e:
+            print(('-' * x) + '*' + ('-' * (7 - x)))
+        sols += 1
+        return
